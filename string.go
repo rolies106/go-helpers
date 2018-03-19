2ff4c0a9478eec509be77a0d8e12a0768fa4f7c2
Rolies Deby <rolies106@gmail.com>
2018-03-19 16:25:49 +0700

    Add string helpers

---
+++ string.go
@@ -0,0 +1,28 @@
+package gohelpers
+
+import (
+	"fmt"
+	"strings"
+)
+
+type String struct {}
+
+/**
+ * Convert kebab-case into CamelCase
+ */
+func kebabToCamelCase(kebab string) (camelCase string) {
+	isToUpper := true
+	for _, runeValue := range kebab {
+		if isToUpper {
+			camelCase += strings.ToUpper(string(runeValue))
+			isToUpper = false
+		} else {
+			if runeValue == '-' {
+				isToUpper = true
+			} else {
+				camelCase += string(runeValue)
+			}
+		}
+	}
+	return
+}
